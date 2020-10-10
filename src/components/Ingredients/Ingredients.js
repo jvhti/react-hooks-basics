babd7102c6b7408bf28469fb1f40c641d1c393c3
jvhti <jvhti@hotmail.com>
2020-10-10 19:02:34 -0300

    Handling errors

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,10 +3,12 @@ import React, {useCallback, useState} from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
 import Search from './Search';
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const addIngredientHandler = ingredient => {
     setIsLoading(true);
@@ -18,8 +20,11 @@ function Ingredients() {
       return response.json();
     }).then(responseData => {
       setIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
+    }).catch(error => {
+      setError("Something went wrong!");
+    }).finally(() => {
       setIsLoading(false);
-    })
+    });
   };
 
   const removeIngredientHandler = id => {
@@ -28,6 +33,9 @@ function Ingredients() {
       method: 'DELETE'
     }).then(() => {
       setIngredients(prevIngredients => prevIngredients.filter(x => x.id !== id));
+    }).catch(error => {
+      setError("Something went wrong!");
+    }).finally(() => {
       setIsLoading(false);
     });
   }
@@ -36,8 +44,11 @@ function Ingredients() {
     setIngredients(filteredIngredients);
   }, []);
 
+  const clearError = () => setError(null);
+
   return (
       <div className="App">
+        {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
         <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
         <section>
