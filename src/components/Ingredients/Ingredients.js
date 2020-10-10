8b0938f9fd75af404edce7c8b79eae9f3ae46efd
jvhti <jvhti@hotmail.com>
2020-10-10 18:58:22 -0300

    Show spinner when adding and removing the ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,8 +6,10 @@ import Search from './Search';
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://ingredients-list-a6589.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
@@ -16,14 +18,17 @@ function Ingredients() {
       return response.json();
     }).then(responseData => {
       setIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
+      setIsLoading(false);
     })
   };
 
   const removeIngredientHandler = id => {
+    setIsLoading(true);
     fetch(`https://ingredients-list-a6589.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE'
     }).then(() => {
       setIngredients(prevIngredients => prevIngredients.filter(x => x.id !== id));
+      setIsLoading(false);
     });
   }
 
@@ -33,7 +38,7 @@ function Ingredients() {
 
   return (
       <div className="App">
-        <IngredientForm onAddIngredient={addIngredientHandler}/>
+        <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
         <section>
           <Search onLoadIngredients={filteredIngredientsHandler}/>
