467b9d046994d9c8ea003d23734806d3e490c534
jvhti <jvhti@hotmail.com>
2020-10-10 13:56:38 -0300

    Use useState multiple times in the same component

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(() => {
-  const [inputState, setInputState] = useState({title: '', amount: ''});
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
@@ -12,22 +13,17 @@ const IngredientForm = React.memo(() => {
   };
 
   return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
+      <section className="ingredient-form">
+        <Card>
+          <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title} onChange={event => {
-              const newTitle = event.target.value;
-              setInputState((prevState) => ({...prevState, title: newTitle}));
-            }}/>
+            <input type="text" id="title" value={enteredTitle} onChange={event => setEnteredTitle(event.target.value)}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount} onChange={event => {
-              const newAmount = event.target.value;
-              setInputState((prevState) => ({...prevState, amount: newAmount}));
-            }}/>
+            <input type="number" id="amount" value={enteredAmount}
+                   onChange={event => setEnteredAmount(event.target.value)}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
