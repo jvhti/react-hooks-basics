f29fe0c3dbf0de5245da56f578dbf682d5cdc198
jvhti <jvhti@hotmail.com>
2020-10-10 18:20:13 -0300

    Add Ingredients to the list

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -3,13 +3,13 @@ import React, {useState} from 'react';
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
-const IngredientForm = React.memo(() => {
+const IngredientForm = React.memo((props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount});
   };
 
   return (
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from "./IngredientList";
 import Search from './Search';
 
 function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+  const [ingredients, setIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
+  const addIngredientHandler = ingredient => {
+    setIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
+  };
+
+  return (
+      <div className="App">
+        <IngredientForm onAddIngredient={addIngredientHandler}/>
+
+        <section>
+          <Search/>
+          <IngredientList ingredients={ingredients} onRemoveItem={() => {
+          }}/>
+        </section>
+      </div>
   );
 }
 
