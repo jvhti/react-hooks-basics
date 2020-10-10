1e4d98c015dc90fb2ed8c1cbb03a596a40759fe4
jvhti <jvhti@hotmail.com>
2020-10-10 20:08:05 -0300

    Use custom hook (useHttp) in the Search component

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -2,11 +2,14 @@ import React, {useEffect, useRef, useState} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
+import useHttp from "../../hooks/http";
+import ErrorModal from "../UI/ErrorModal";
 
 const Search = React.memo(props => {
   const {onLoadIngredients} = props;
   const [filter, setFilter] = useState('');
   const inputRef = useRef();
+  const {isLoading, error, data, sendRequest, clear} = useHttp();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -14,24 +17,30 @@ const Search = React.memo(props => {
 
       const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
 
-      fetch('https://ingredients-list-a6589.firebaseio.com/ingredients.json' + query).then(res => res.json()).then(res => {
-        const loadedIngredients = [];
-        for (const key in res) {
-          loadedIngredients.push({id: key, title: res[key].title, amount: res[key].amount});
-        }
-
-        onLoadIngredients(loadedIngredients);
-      });
+      sendRequest('https://ingredients-list-a6589.firebaseio.com/ingredients.json' + query, 'GET');
     }, 500);
 
     return () => clearTimeout(timer);
-  }, [filter, onLoadIngredients, inputRef]);
+  }, [filter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if (isLoading || error || !data) return;
+    const loadedIngredients = [];
+    for (const key in data) {
+      loadedIngredients.push({id: key, title: data[key].title, amount: data[key].amount});
+    }
+
+    onLoadIngredients(loadedIngredients);
+
+  }, [data, isLoading, error, onLoadIngredients]);
 
   return (
       <section className="search">
+        {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
         <Card>
           <div className="search-input">
             <label>Filter by Title</label>
+            {isLoading && <span>Loading...</span>}
             <input ref={inputRef} type="text" value={filter} onChange={event => setFilter(event.target.value)}/>
           </div>
         </Card>
